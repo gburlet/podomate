909148765e00c0d44da56d7cb738da5539f6c2a5
Greg Burlet <gregory.burlet@gmail.com>
2020-11-11 17:33:57 -0700

    Cleanup deplosive filter
    clean up, clean up, everybody clean up

--- podomatecli/audio_preprocessors/deplosive_filter.py
+++ podomatecli/audio_preprocessors/deplosive_filter.py
@@ -2,8 +2,6 @@ import librosa
 import numpy as np
 from scipy.signal import butter, filtfilt
 
-from utils import time_to_timestamp
-
 
 class DeplosiveFilter(object):
     """
@@ -13,28 +11,33 @@ class DeplosiveFilter(object):
     Attenuate: reduce the dB of the plosive regions
     """
 
-    def __init__(self, window_size=1024, hop_size=1024, order=5, cutoff=225, db_attenuation=-3.5, zero_cross_threshold=0.2, rms_threshold=1.5, min_plosive_length_s=0.03, max_plosive_length_s=0.2):
+    PRE_LPFILTER_CUTOFF = 800
+    PRE_LPFILTER_ORDER = 5
+    VAD_RMS_PERCENTILE = 30         # %ile to set threshold for voice activity RMS
+    PUNCHY_RMS_PERCENTILE = 85      # %ile to set threshold for intense (punchy) voice activity
+    PLOSIVE_0X_PERCENTILE = 30      # %ile to set threshold for plosive
+    DEFAULT_RMS_MEAN = 0.04
+    RMS_MOVING_AVG_FRAME_LAG = 5    # number of frames to calculate in RMS moving average
+    PLOSIVE_HPFILTER_ORDER = 5
+    HIGH_ENERGY_RMS_MULTIPLIER = 1.75
+    ENERGY_BURST_RMS_PERCENT_DIFF = 1.0
+
+    def __init__(self, window_size=1024, hop_size=1024, cutoff=225, db_attenuation=-3.5, min_plosive_length_s=0.03, max_plosive_length_s=0.2):
         """
         Parameters
         ----------
         window_size (int): analysis window size in samples
         hop_size (int): hop size in samples
-        order (int): filter order
-        cutoff (int): high-pass filter cut-off frequency
+        cutoff (int): plosive high-pass filter cut-off frequency
         db_attenuation (float): dB plosive attenuation after high-pass filtering
-        zero_cross_threshold (float): proportion of track's mean 0x to tag plosives
-        rms_threshold (float): proportion of track's mean rms to tag plosives
         min_plosive_length_s (float): any region tagged as a plosive shorter than this, skip it
         max_plosive_length_s (float): any region tagged as a plosive longer than this, skip it, prob not a plosive
         """
 
         self._window_size = window_size
         self._hop_size = hop_size
-        self._order = order
-        self._cutoff = cutoff
+        self._plosive_cutoff_freq = cutoff
         self._db_attenuation = db_attenuation
-        self._zero_cross_threshold = zero_cross_threshold
-        self._rms_threshold = rms_threshold
         self._min_plosive_length_s = min_plosive_length_s
         self._max_plosive_length_s = max_plosive_length_s
 
@@ -44,161 +47,71 @@ class DeplosiveFilter(object):
         ----------
         track (Track): track to remove plosives from
 
-        Algorithm
-        - what we're looking for: low zero-crossing and relatively high volume
+        Algorithm: we're looking for short areas with low zero-crossing and relatively high volume
         ---------
-        1. Remove any DC offset
-        2. Get sample of 0x speaker activities
-        3. Get sample of RMS noise floor
-        4. Tag plosives in frames where 0x < 0.5*0x_activity, RMS > 2*RMS_noisefloor
-        5. Apply 150Hz cut-off freq high-pass filter to plosive regions
+        1. Low-pass filter @800Hz to remove excessive 0x in time domain interfering with analysis
+        2. Remove any DC offset
+        3. Calculate 0x and RMS thresholds for windowed signal
+        4. Tag frames where plosive detected:
+            low 0x & sudden burst in signal energy over moving average or just high signal energy
+        # 5. Flood fill plosive tag gaps [Yes, No, Yes] -> [Yes, Yes, Yes]
+        # 6. Convert plosive tags to regions at nearest 0xings
+            Note: Operating on audio within 0xings removes clicks when applying filters
+        # 7. Merge any overlapped regions resulting from analysis window hopping
+        # 8. Perform high-pass filter & dB reduction on plosive to remove high impact breath hitting mic
 
         Note: modifies track AudioBuffer in place!
         """
 
-        """
-        # 1. Remove any DC offset
-        dc_offset = np.mean(track.audio_buffer.x)
-        if abs(dc_offset) > 0.001:
-            track.audio_buffer.x += dc_offset
-        """
-
-        # low-pass filter to smooth
-        # TODO: downsample for analysis
-        b, a = butter(5, 800, btype='low', fs=track.audio_buffer.fs)
+        # 1. Low-pass filter to smooth
+        b, a = butter(
+            DeplosiveFilter.PRE_LPFILTER_ORDER, DeplosiveFilter.PRE_LPFILTER_CUTOFF, btype='low', fs=track.audio_buffer.fs
+        )
         x_smoothed = filtfilt(b, a, track.audio_buffer.x)
 
-        # remove any DC offset from low-pass smoothed signal
+        # 2. Remove any DC offset from low-pass smoothed signal
         dc_offset = np.mean(x_smoothed)
         if abs(dc_offset) > 0.001:
             x_smoothed += dc_offset
 
-        # sample noisefloor RMS
-        """
-        silent_regions = track.silence_range_cache if track.silence_range_cache else track.silence_ranges
-        mean_rms = 0.04
-        if len(silent_regions):
-            mean_rms = 0.
-            for silent_region in silent_regions:
-                silent_sample_start = track.audio_buffer.get_sample_from_timestamp(silent_region[0])
-                silent_sample_end = track.audio_buffer.get_sample_from_timestamp(silent_region[1])
-                region_rms = np.mean(
-                    librosa.feature.rms(
-                        x_smoothed[silent_sample_start:silent_sample_end],
-                        frame_length=self._window_size, hop_length=self._hop_size, center=False
-                    )
-                )
-                mean_rms += region_rms
-            mean_rms /= float(len(silent_regions))
-        """
+        # 3. Calculate 0x and RMS thresholds for windowed signal
+        frame_rms = np.squeeze(librosa.feature.rms(
+            x_smoothed, frame_length=self._window_size, hop_length=self._hop_size, center=False
+        ))
+        vad_rms_threshold = np.percentile(frame_rms, DeplosiveFilter.VAD_RMS_PERCENTILE)
+        punchy_rms_threshold = np.percentile(frame_rms, DeplosiveFilter.PUNCHY_RMS_PERCENTILE)
+        zero_x_rate = np.squeeze(librosa.feature.zero_crossing_rate(
+            x_smoothed, frame_length=self._window_size, hop_length=self._hop_size, center=False
+        ))
+        plosive_0x_threshold = np.percentile(
+            zero_x_rate[frame_rms > vad_rms_threshold], DeplosiveFilter.PLOSIVE_0X_PERCENTILE
+        )
 
-        frame_rms = librosa.feature.rms(x_smoothed, frame_length=self._window_size, hop_length=self._hop_size, center=False)
-        vad_rms_threshold = np.percentile(frame_rms, 30)
-        punchy_rms_threshold = np.percentile(frame_rms, 85)
-        #noisefloor_rms = np.mean(frame_rms[frame_rms < vad_rms_threshold])
-        print("vad rms thresh: %0.3f" % vad_rms_threshold)
-        print("punchy rms thresh: %0.3f" % punchy_rms_threshold)
-        zero_x_rate = librosa.feature.zero_crossing_rate(x_smoothed, frame_length=self._window_size, hop_length=self._hop_size, center=False)
-        plosive_0x_threshold = np.percentile(zero_x_rate[frame_rms > vad_rms_threshold], 30)
-        print("plosive 0x threshold: %0.3f" % plosive_0x_threshold)
-        vad_0x_threshold = np.percentile(zero_x_rate[frame_rms > vad_rms_threshold], 70)
-        mean_0x_rate = np.mean(zero_x_rate[zero_x_rate > vad_0x_threshold])
-        print("mean active 0x rate: %0.3f" % mean_0x_rate)
-
-        """
-        # 2. Sample zero crossing profile
-        # mean_zero_crossings = int(np.sum(librosa.zero_crossings(track.audio_buffer.x))) / float(len(track.audio_buffer.x)/self._window_size)
-        activity_ranges = track.activity_range_cache if track.activity_range_cache else track.activity_ranges
-        print("activity ranges: ", activity_ranges)
-        mean_zero_crossings = 30 * self._window_size / 1024.  # default value set by looking at some tracks
-        mean_zero_crossing_rate = 0.02
-        if len(activity_ranges):
-            mean_zero_crossings = 0.
-            mean_zero_crossing_rate = 0.
-            for activity_range in activity_ranges:
-                activity_sample_start = track.audio_buffer.get_sample_from_timestamp(activity_range[0])
-                activity_sample_end = track.audio_buffer.get_sample_from_timestamp(activity_range[1])
-                activity_num_samples = activity_sample_end - activity_sample_start
-                region_zero_crossings = int(
-                    np.sum(librosa.zero_crossings(x_smoothed[activity_sample_start:activity_sample_end])))
-                mean_zero_crossings += (region_zero_crossings / (activity_num_samples / float(self._window_size)))
-                mean_zero_crossing_rate += region_zero_crossings / float(activity_num_samples)
-            mean_zero_crossings /= float(len(activity_ranges))
-            mean_zero_crossing_rate /= float(len(activity_ranges))
-
-        # mean_zero_crossings = 15 * self._window_size/512.
-        # mean_zero_crossings = 15. / self._zero_cross_threshold
-        print("mean 0x: %0.3f" % mean_zero_crossings)
-        print("mean 0x rate: %0.3f" % mean_zero_crossing_rate)
-        """
-
-        # 3. Sample RMS profile
-        # mean_rms = np.mean(librosa.feature.rms(track.audio_buffer.x))
-        """
-        silent_regions = track.silence_range_cache if track.silence_range_cache else track.silence_ranges
-        mean_rms = 0.04
-        if len(silent_regions):
-            mean_rms = 0.
-            for silent_region in silent_regions:
-                silent_sample_start = track.audio_buffer.get_sample_from_timestamp(silent_region[0])
-                silent_sample_end = track.audio_buffer.get_sample_from_timestamp(silent_region[1])
-                region_rms = np.mean(
-                    librosa.feature.rms(
-                        x_smoothed[silent_sample_start:silent_sample_end],
-                        frame_length=self._window_size, hop_length=self._hop_size, center=False
-                    )
-                )
-                mean_rms += region_rms
-            mean_rms /= float(len(silent_regions))
-        """
-        #mean_rms = 0.04
-        #print("mean rms noisefloor: %0.3f" % mean_rms)
-
-        # tag plosive frames
+        # 4. Tag plosive frames
         frames = librosa.util.frame(
             x_smoothed, frame_length=self._window_size, hop_length=self._hop_size
         )
         num_frames = np.shape(frames)[1]
         is_plosive = np.zeros(num_frames, dtype=np.bool)
-        prev_0x_rate = 0.
         for i_frame in range(num_frames):
-            zero_crossings = int(np.sum(librosa.zero_crossings(frames[:, i_frame])))
-            zero_crossing_rate = zero_crossings / float(np.shape(frames[:, i_frame])[0])
-            prev_frames_rms_mean = float(np.mean(
-                np.sqrt(np.mean(np.abs(frames[:, max(0,i_frame-5):i_frame]) ** 2, axis=0, keepdims=True)), axis=1
-            )) if i_frame > 0 else 0.04
-            #print("prev_frames_rms_mean: %0.3f" % prev_frames_rms_mean)
-            rms = float(librosa.feature.rms(frames[:, i_frame], frame_length=self._window_size, hop_length=self._hop_size, center=False))
-            # debug
-            print("Frame: @[%s - %s]: 0x: %d; 0xrate: %0.3f; rms: %0.3f; ma_rms: %0.3f" % (
-                time_to_timestamp((i_frame * self._hop_size) / float(track.audio_buffer.fs)),
-                time_to_timestamp(((i_frame * self._hop_size) + self._window_size) / float(track.audio_buffer.fs)),
-                zero_crossings, zero_crossing_rate, rms, prev_frames_rms_mean
-            ))
+            zero_crossing_rate = zero_x_rate[i_frame]
+            rms = frame_rms[i_frame]
+            moving_avg_rms_mean = float(np.mean(
+                frame_rms[max(0,i_frame-DeplosiveFilter.RMS_MOVING_AVG_FRAME_LAG):i_frame]
+            )) if i_frame > 0 else DeplosiveFilter.DEFAULT_RMS_MEAN
 
-            if (rms-prev_frames_rms_mean)/prev_frames_rms_mean > 1.0:
-                print("BIG BURST")
+            rms_percent_diff = (rms-moving_avg_rms_mean) / moving_avg_rms_mean
+            is_low_0x = zero_crossing_rate <= plosive_0x_threshold
+            is_energy_burst = rms > punchy_rms_threshold and rms_percent_diff > DeplosiveFilter.ENERGY_BURST_RMS_PERCENT_DIFF
+            is_high_energy = rms > DeplosiveFilter.HIGH_ENERGY_RMS_MULTIPLIER*punchy_rms_threshold and rms_percent_diff > 0.
+            is_plosive[i_frame] = is_low_0x and (is_energy_burst or is_high_energy)
 
-            # plosive detected
-            #is_plosive[i_frame] = zero_crossings <= self._zero_cross_threshold*mean_zero_crossings and rms > self._rms_threshold*mean_rms
-            #is_plosive[i_frame] = zero_crossing_rate <= plosive_0x_threshold and rms > punchy_rms_threshold
-            prev_is_plosive = is_plosive[i_frame-1] if i_frame-1 >= 0 else False
-
-            rms_percent_diff = (rms-prev_frames_rms_mean)/prev_frames_rms_mean
-            is_plosive[i_frame] = zero_crossing_rate <= plosive_0x_threshold and ((rms > punchy_rms_threshold and rms_percent_diff > 1.0) or (rms > 1.75*punchy_rms_threshold and rms_percent_diff > 0.))
-            # if is_plosive[i_frame] and not prev_is_plosive:
-            #     is_plosive[i_frame] = (zero_crossing_rate-prev_0x_rate)/prev_0x_rate < 0.
-            if is_plosive[i_frame]:
-                print("\tplosive detected")
-
-            prev_0x_rate = zero_crossing_rate
-
-        # flood fill plosive gaps [Yes, No, Yes] -> [Yes, Yes, Yes]
+        # 5. Flood fill plosive gaps [Yes, No, Yes] -> [Yes, Yes, Yes]
         for i_frame in range(1, num_frames-1):
             if is_plosive[i_frame-1] and is_plosive[i_frame+1]:
                 is_plosive[i_frame] = True
 
-        # convert plosive tags to regions at nearest 0xings (list of sample indices)
+        # 6. Convert plosive tags to sample regions at nearest 0xings (list of sample indices)
         plosive_regions = []
         current_plosive = None
         for i_frame in range(num_frames):
@@ -222,16 +135,7 @@ class DeplosiveFilter(object):
             if self._min_plosive_length_s <= plosive_duration_s <= self._max_plosive_length_s:
                 plosive_regions.append(current_plosive)
 
-        # debug
-        print("Detected Plosives before 0x snapping:")
-        for plosive in plosive_regions:
-            print("\t[%s - %s]" % (
-                time_to_timestamp(track.audio_buffer.get_timestamp_from_sample(plosive[0])),
-                time_to_timestamp(track.audio_buffer.get_timestamp_from_sample(plosive[1]))
-            )
-        )
-
-        # convert end points of plosive regions to 0xings
+        # convert sample end points of plosive regions to 0xings
         plosive_regions = [
             [
                 track.audio_buffer.get_previous_zero_crossing(pr[0], zero_tol=1e-2, sign_change=False),
@@ -239,59 +143,18 @@ class DeplosiveFilter(object):
             ] for pr in plosive_regions
         ]
 
-        # merge any overlapped regions
+        # 7. Merge any overlapped regions
         for i_plosive in range(len(plosive_regions)-1,0,-1):
             if plosive_regions[i_plosive][0] < plosive_regions[i_plosive-1][1]:
                 plosive_regions[i_plosive-1][1] = plosive_regions[i_plosive][1]
                 del plosive_regions[i_plosive]
 
-        # debug
-        print("Detected Plosives:")
-        for plosive in plosive_regions:
-            print("\t[%s - %s]" % (
-                    time_to_timestamp(track.audio_buffer.get_timestamp_from_sample(plosive[0])),
-                    time_to_timestamp(track.audio_buffer.get_timestamp_from_sample(plosive[1]))
-                )
-            )
-
-        # perform filter: 5 order, 200Hz high-pass filter
-        b, a = butter(self._order, self._cutoff, btype='high', fs=track.audio_buffer.fs)
+        # 8. Perform high-pass filter & dB reduction on plosive to remove high impact breath hitting mic
+        b, a = butter(
+            DeplosiveFilter.PLOSIVE_HPFILTER_ORDER, self._plosive_cutoff_freq, btype='high', fs=track.audio_buffer.fs
+        )
         for plosive in plosive_regions:
             track.audio_buffer.x[plosive[0]:plosive[1]] = filtfilt(
                 b, a, track.audio_buffer.x[plosive[0]:plosive[1]]
             )
             track.audio_buffer.x[plosive[0]:plosive[1]] *= np.power(10, self._db_attenuation/20.)
-        #track.audio_buffer.x = x_smoothed
-
-
-if __name__ == "__main__":
-    from track import Track
-
-    # plosives @: 1.856, 2.409, 6.258, 11.029, 15.325, 19.656, 21.233, 22.808, 25.247, 27.526
-    # audio_path = "/Users/gburlet/Podomate/plosives_example3.mp3"
-    # track = Track.from_audio_file(audio_path, master=True)
-    # track.audio_buffer.normalize()
-    # w = 1536
-    # h = 512
-    #
-    # df = DeplosiveFilter(w, h)
-    # df.process(track)
-    #
-    # audio_out_path = "/Users/gburlet/Podomate/plosives_example3_clean.flac"
-    # track.audio_buffer._path = audio_out_path
-    # track.audio_buffer.write(normalize=False)
-
-    for i in range(1,7):
-        audio_path = "/Users/gburlet/Podomate/plosives_example%d.mp3" % i
-        print("\n\n\n", audio_path)
-        track = Track.from_audio_file(audio_path, master=True)
-        track.audio_buffer.normalize()
-        w = 1536
-        h = 512
-
-        df = DeplosiveFilter(w, h)
-        df.process(track)
-
-        audio_out_path = "/Users/gburlet/Podomate/plosives_example%d_clean.flac" % i
-        track.audio_buffer._path = audio_out_path
-        track.audio_buffer.write(normalize=False)
